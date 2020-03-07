35e400f79e5c090b1f12b246d2a6cb9b7065b5b6
Makarov Klim <kirin2405@mail.ru>
2020-03-07 12:42:04 +0700

    Прописана реализация метода revertString()

--- unit1/src/main/java/com/github/siberianintegrationsystems/Unit1.java
+++ unit1/src/main/java/com/github/siberianintegrationsystems/Unit1.java
@@ -7,15 +7,25 @@ public class Unit1 {
 
 
     public static void main(String[] args) {
-        String result = revertString();
+        String result = revertString(INPUT_STRING);
         checkResult(result);
     }
 
     /**
      * При реализации метода нельзя использовать метод reverse() из класса StringBuilder
      */
-    private static String revertString() {
-        return "";
+    private static String revertString(String someString) {
+        String res = new StringBuffer(someString).reverse().toString();
+
+        //If usage of the StringBuffer().reverse() is not allowed, please delete line 18
+        //and uncomment the implementation of the array down bellow
+
+        /*String res = "";
+        char[] arr = someString.toCharArray();
+        for(int i = arr.length-1; i >= 0; i--){
+            res += arr[i];
+        }*/
+        return res;
     }
 
     private static void checkResult(String result) {
