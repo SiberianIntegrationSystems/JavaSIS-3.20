751e7644d38553cc0b61e1929ff9b9d664685789
Ilya <Korobeynikovi@yandex.ru>
2020-03-11 21:58:45 +0700

    Написал функцию

--- unit1/src/main/java/com/github/siberianintegrationsystems/Unit1.java
+++ unit1/src/main/java/com/github/siberianintegrationsystems/Unit1.java
@@ -2,20 +2,35 @@ package com.github.siberianintegrationsystems;
 
 public class Unit1 {
 
-    private static final String INPUT_STRING = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
-        + "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+private static final String INPUT_STRING = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+            + "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
 
     public static void main(String[] args) {
-        String result = revertString();
-        checkResult(result);
+        String result = INPUT_STRING;
+        String g = revertString(result);
+        checkResult(g);
     }
 
     /**
      * При реализации метода нельзя использовать метод reverse() из класса StringBuilder
      */
-    private static String revertString() {
-        return "";
+
+    private static String revertString(String inputString) {
+        String rightPart;
+        String leftPart;
+
+        int length = inputString.length();
+
+        if (length <= 1) {
+            return inputString;
+        }
+
+        leftPart = inputString.substring(0, length / 2);
+
+        rightPart = inputString.substring(length / 2, length);
+
+        return revertString(rightPart) + revertString(leftPart);
     }
 
     private static void checkResult(String result) {
